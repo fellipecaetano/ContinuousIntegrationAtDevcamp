dce43042dbf0adb221233f309e43f0424f19cd61
Tomas Queiroga <tomas.queiroga@dextra-sw.com>
2014-05-14 20:55:53 -0300

    Hard-coded list of images (it is just a presentation anyway)

--- ContinuousIntegrationAtDevCamp/GalleryViewController.m
+++ ContinuousIntegrationAtDevCamp/GalleryViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "GalleryViewController.h"
+#import <Social/Social.h>
+#import <Accounts/Accounts.h>
 
 @interface GalleryViewController ()
 @end
@@ -22,7 +24,14 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSLog(@"Gallery loaded!");
+    
+    NSArray* images = @[
+        @"https://pbs.twimg.com/profile_images/466690236500557824/g0JKsTn_.jpeg",
+        @"https://pbs.twimg.com/profile_images/466626081470955520/RewkXkPC.png",
+        @"https://pbs.twimg.com/profile_images/2634755453/40ffdf8a114b08a13aecc6f2a3a17320.png",
+        @"https://pbs.twimg.com/profile_images/1733661436/41813_104538479599168_2496_n.jpg"
+    ];
+    NSLog(@"Images: %@", images);
 }
 
 - (void)didReceiveMemoryWarning
