de4885366a353dcf7f4344a7fa35aa1b919fa7c2
Fellipe Caetano <fellipe.caetano4@gmail.com>
2014-05-17 02:20:07 -0300

    Our keychain manipulation must define a default keychain so that Xcode can find our keys during our builds

--- Scripts/travis/clear_keychain.sh
+++ Scripts/travis/clear_keychain.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 security delete-keychain ios-build.keychain
+security default-keychain -s login.keychain
 rm -rf ~/Library/MobileDevice/Provisioning\ Profiles/$PROFILE_NAME.mobileprovision
--- Scripts/travis/setup_keychain.sh
+++ Scripts/travis/setup_keychain.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 security create-keychain -p travis ios-build.keychain
+security default-keychain -s ios-build.keychain
 security import ./Scripts/travis/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./Scripts/travis/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./Scripts/travis/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign -P "$DISTRIBUTION_KEY_PASSWORD"
