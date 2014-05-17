d147332e48cf6ffcf645d4c4228bacbb46986ccb
Fellipe Caetano <fellipe.caetano4@gmail.com>
2014-05-17 01:24:04 -0300

    Since we're going to need to test our app and then create a package out of it, let's separate those two tasks into their own script files

---
+++ Scripts/travis/create_package.sh
@@ -0,0 +1,2 @@
+xctool -workspace ContinuousIntegrationAtDevCamp.xcworkspace -scheme ContinuousIntegrationAtDevCamp -sdk iphoneos -configuration Release clean build
+OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO
---
+++ Scripts/travis/test.sh
@@ -0,0 +1,1 @@
+xctool -workspace ContinuousIntegrationAtDevCamp.xcworkspace -scheme ContinuousIntegrationAtDevCamp -sdk iphonesimulator7.1 clean build test -freshInstall
